1d8a903bda21356b9db4baacc5e1560234927f49
= <tmntbug2011@gmail.com>
2026-09-14 22:07:38 -0500

    Updated Flake

--- flake.nix
+++ flake.nix
@@ -3,9 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+    zen-browser.url = "github:youwen5/zen-browser-flake";
 
-  outputs = { self, nixpkgs }: {
+    # optional, but recommended if you closely follow NixOS unstable so it shares
+    # system libraries, and improves startup time
+    # NOTE: if you experience a build failure with Zen, the first thing to check is to remove this line!
+    zen-browser.inputs.nixpkgs.follows = "nixpkgs";
+    # ...
+};
+  outputs = { self, nixpkgs, zen-browser }: {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
          modules = [ ./configuration.nix ];
     };
